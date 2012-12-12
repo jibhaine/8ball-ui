b922b56d4be2d85b1d618453be9df33b8bc858ae
Benjamin Legrand <jibhaine@gmail.com>
2012-12-12 12:54:01 +0100

    ignore and files

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'db' => array(
+        'driver'   => 'pdo_mysql',
+        'host'     => '127.0.0.1',
+        'dbname'   => '8ball',
+        'user'     => '8ball',
+        'password' => '8ball',
+    ),
+
+    'misc' => array(
+        'debug'    => true,
+    ),
+);
---
+++ web/js/8ball.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Piotr Walczyszyn (@pwalczyszyn)
+ *
+ * User: pwalczys
+ * Date: 2/16/12
+ * Time: 9:20 AM
+ */
+
+require.config({
+    paths:{
+        // RequireJS plugin
+        text:'libs/require/text',
+        // RequireJS plugin
+        domReady:'libs/require/domReady',
+        // underscore library
+        underscore:'libs/underscore/underscore',
+        // Backbone.js library
+        Backbone:'libs/backbone/backbone',
+        // jQuery
+        jquery:'libs/jquery/jquery-1.8.2',
+        //hammer
+        hammer:'libs/hammer/hammer',
+        "jquery-hammer":'libs/hammer/jquery.hammer',
+        //D3
+        d3:'libs/d3_v2',
+        // backvbone-d3
+        "backbone-d3":'libs/backbone/backbone-d3'
+    },
+    shim:{
+        Backbone:{
+            deps:['underscore', 'jquery'],
+            exports:'Backbone'
+        },
+        underscore:{
+            exports:'_'
+        },
+        "backbone-d3":{
+            deps:['Backbone', 'd3']
+        }
+    }
+});
+
+require(['domReady', 'views/home/HomeView','backbone-d3'],
+    function (domReady, HomeView) {
+
+        // domReady is RequireJS plugin that triggers when DOM is ready
+        domReady(function () {
+
+            function onDeviceReady(desktop) {
+                // Hiding splash screen when app is loaded
+                if (desktop !== true)
+                    cordova.exec(null, null, 'SplashScreen', 'hide', []);
+
+                // Setting jQM pageContainer to #container div, this solves some jQM flickers & jumps
+                // I covered it here: http://outof.me/fixing-flickers-jumps-of-jquery-mobile-transitions-in-phonegap-apps/
+                //$.mobile.pageContainer = $('#container');
+
+                // Setting default transition to slide
+               // $.mobile.defaultPageTransition = 'slide';
+
+                // Pushing MainView
+                //$.mobile.jqmNavigator.pushView(new HomeView());
+            }
+
+            if (navigator.userAgent.match(/(iPad|iPhone|Android)/)) {
+                // This is running on a device so waiting for deviceready event
+                document.addEventListener('deviceready', onDeviceReady, false);
+            } else {
+                // On desktop don't have to wait for anything
+                onDeviceReady(true);
+            }
+
+        });
+
+    });
